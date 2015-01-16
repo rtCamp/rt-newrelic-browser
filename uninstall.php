0d6ee48b40e3679f7cee5af74bb44129d5e559ca
Rohan Veer <rohan.veer@rtcamp.com>
2015-01-16 13:11:58 +0530

    updated Uninstall.php,

--- uninstall.php
+++ uninstall.php
@@ -20,15 +20,15 @@ $option_name = 'rtp_relic_account_details';
 $app_option_name = 'rtp_relic_browser_details';
 $browser_app_list = 'rtp_relic_browser_list';
 
-if ( false != get_option( $option_name, false ) ) {
+if ( get_option( $option_name, false ) ) {
 	delete_option( $option_name );
 	delete_site_option( $option_name );
 }
-if ( false != get_option( $app_option_name, false ) ) {
+if ( get_option( $app_option_name, false ) ) {
 	delete_option( $app_option_name );
 	delete_site_option( $app_option_name );
 }
-if ( false != get_option( $browser_app_list, false ) ) {
+if ( get_option( $browser_app_list, false ) ) {
 	delete_option( $browser_app_list );
 	delete_site_option( $browser_app_list );
 }
